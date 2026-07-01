109fccf580377c27d4a624248fc8c1a7618f490a
franciscosousa <80540828+CiscG@users.noreply.github.com>
2026-07-01 00:48:34 -0300

    feat: adicionar validações no ReservaRequest

--- src/main/java/com/airbnbclone/dto/ReservaRequest.java
+++ src/main/java/com/airbnbclone/dto/ReservaRequest.java
@@ -1,8 +1,17 @@
 package com.airbnbclone.dto;
 
+import jakarta.validation.constraints.NotBlank;
+
 public record ReservaRequest(
+        @NotBlank(message = "propriedadeId não pode ser vazio")
         String propriedadeId,
+        
+        @NotBlank(message = "hospedeId não pode ser vazio")
         String hospedeId,
+        
+        @NotBlank(message = "checkin não pode ser vazio")
         String checkin,
+        
+        @NotBlank(message = "checkout não pode ser vazio")
         String checkout
 ) {}
