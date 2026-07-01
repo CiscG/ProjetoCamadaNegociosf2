75b0104fca98057f64906135ffa57af8c0cd6da7
franciscosousa <80540828+CiscG@users.noreply.github.com>
2026-07-01 01:35:30 -0300

    fix: adicionar anotação JsonProperty na PropriedadeRequest

--- src/main/java/com/airbnbclone/dto/PropriedadeRequest.java
+++ src/main/java/com/airbnbclone/dto/PropriedadeRequest.java
@@ -1,13 +1,16 @@
 package com.airbnbclone.dto;
 
 import com.airbnbclone.model.Endereco;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import java.util.List;
 
 public record PropriedadeRequest(
+        @JsonProperty("anfitriaoId")
         String anfitriaoId,
         String titulo,
         String descricao,
+        @JsonProperty("precoPorNoite")
         Double precoPorNoite,
         Endereco endereco,
         List<String> comodidades
